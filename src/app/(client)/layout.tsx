9e6da691013bf95ae0e9224d52d1c08f32998428
Gianfranco <gianfranco227@hotmail.com>
2026-05-05 00:02:42 -0400

    feat(client): add initial header layout structure

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -3,5 +3,28 @@ export default function ClientLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <>
+      <header>
+        <div>
+          <small>Address Placeholder</small>
+          <small>Phone Number Placeholder</small>
+        </div>
+        <nav>
+          <div>Logo Placeholder</div>
+          <ul>
+            <li>HOME</li>
+            <li>VINYL WRAPS</li>
+            <li>WINDOW TINTS</li>
+            <li>PPF</li>
+            <li>CERAMIC COUTING</li>
+            <li>CONTACT</li>
+            <li>ABOUT US</li>
+          </ul>
+          <button>Services Button</button>
+        </nav>
+      </header>
+      <section>{children}</section>
+    </>
+  );
 }
